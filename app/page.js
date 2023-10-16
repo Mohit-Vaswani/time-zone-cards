3dd1e4628f9c91bd1a70983321d83ac926505ad7
Mohit <mohitvaswani010@gmail.com>
2023-10-16 21:18:28 +0530

    basic setup and ui done

--- app/page.js
+++ app/page.js
@@ -1,113 +1,122 @@
-import Image from 'next/image'
+"use client"
+import React, { useState, useEffect } from "react";
+
+// import "normalize.css";
+import styled from "styled-components";
+
+// import "./styles.css";
+import { timezones as tz } from "../data/timezones";
+
+import LocationDisplay from "../components/LocationDisplay";
+import PlaceInput from "../components/PlaceInput";
+
+function lsTest() {
+  var test = "test";
+  try {
+    localStorage.setItem(test, test);
+    localStorage.removeItem(test);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+const Title = styled.h1`
+  color: #e3e3e3;
+  font-weight: lighter;
+  font-size: 2em;
+  text-align: center;
+`;
+
+const Footer = styled.footer`
+  color: lightgray;
+  font-size: 0.8em;
+  /* background: black; */
+  margin-top: 1em;
+  text-align: center;
+  padding: 1em;
+`;
+
+const Container = styled.section`
+  margin: 0 auto;
+  max-width: 800px;
+  padding: 2em;
+`;
+
+const FooterLink = styled.a`
+  color: lightgray;
+  text-decoration: none;
+
+  &:hover {
+    color: white;
+  }
+`;
+
+const Header = styled.header`
+  opacity: ${(props) => (props.showHeader ? 1 : 0)};
+  transition: opacity 1s ease-in;
+`;
 
 export default function Home() {
+  const [clocks, setClocks] = useState([]);
+  const [showHeader, setShowHeader] = useState(true);
+
+  useEffect(() => {
+    if (lsTest() === true) {
+      const hasClocksInStorage =
+        localStorage.getItem("clocks") &&
+        JSON.parse(localStorage.getItem("clocks")).length > 0;
+      const storedClocks = hasClocksInStorage
+        ? JSON.parse(localStorage.getItem("clocks"))
+        : null;
+      if (hasClocksInStorage) {
+        setClocks(storedClocks);
+      }
+    } else {
+      console.log("localStorage not available");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (lsTest() === true) {
+      localStorage.setItem("clocks", JSON.stringify(clocks));
+    } else {
+      console.log("localStorage not available");
+    }
+  }, [clocks]);
+
+  const addClock = (city) => {
+    setClocks([city, ...clocks]);
+  };
+
+  const removeClock = (id) => {
+    setClocks(
+      clocks.filter((city) => city.fields.geonameid !== parseInt(id, 10))
+    );
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="App">
+      <Container>
+        <Header showHeader={showHeader}>
+          <Title>World Clock</Title>
+          <PlaceInput addClock={addClock} />
+        </Header>
+        {clocks &&
+          clocks.map((city) => (
+            <LocationDisplay
+              key={city.id}
+              removeClock={removeClock}
+              city={city}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          ))}
+      </Container>
+      <Footer>
+        World Clock by{" "}
+        <FooterLink href="https://github.com/mohit-vaswani" target="_blank">
+          @mohit-vaswani
+        </FooterLink>{" "}
+      </Footer>
+    </div>
+  );
 }
